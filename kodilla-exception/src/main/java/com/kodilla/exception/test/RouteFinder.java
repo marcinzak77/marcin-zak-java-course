ded7da19cfdd881d03f986230a072e00c9935849
marcinzak77 <marcin.zak77@gmail.com>
2018-05-28 10:31:39 +0200

    8.4

--- kodilla-exception/src/main/java/com/kodilla/exception/test/RouteFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/RouteFinder.java
@@ -7,17 +7,28 @@ public class RouteFinder {
 
     public void findFilght(Flight flight) throws RouteNotFoundException{
 
-        Map<String, Boolean> airportAvailability = new HashMap<>();
+        //Map<String, Boolean> airportAvailability = new HashMap<>();
+        Map<String, Boolean> airportList = new HashMap<>();
+        airportList.put("Berlin", true);
+        airportList.put("Barcelona", true);
+        airportList.put("Praha", true);
+        airportList.put("Moscow", true);
+        airportList.put("Athens", false);
+
         Flight flightDetails = flight;
         String departureAirport = flightDetails.getDepartureAirport();
         String arrivalAirport = flightDetails.getArrivalAirport();
 
-        for (Map.Entry<String, Boolean> entry: airportAvailability.entrySet()) {
-            if (entry.getKey().equals(departureAirport) || entry.getKey().equals(arrivalAirport)) {
-                System.out.println("Flight is available.");
-                break;
+        for (Map.Entry<String, Boolean> entry: airportList.entrySet()) {
+            if (entry.getKey().equals(departureAirport) && entry.getValue()) {
+                for (Map.Entry<String, Boolean> entry2: airportList.entrySet()) {
+                    if (entry2.getKey().equals(arrivalAirport) && entry2.getValue()) {
+                        System.out.println("Flight is available.");
+                        return;
+                    }
                 }
             }
+        }
         throw new RouteNotFoundException("Airport not available!");
     }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/RouteFinderRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/RouteFinderRunner.java
@@ -3,10 +3,20 @@ package com.kodilla.exception.test;
 public class RouteFinderRunner {
     public static void main(String[] args) {
         RouteFinder routeFinder = new RouteFinder();
-        Flight flight1 = new Flight("London", "Paris");
+        Flight flight1 = new Flight("Berlin", "Praha");
 
         try {
             routeFinder.findFilght(flight1);
+        } catch (RouteNotFoundException e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("The end");
+        }
+
+        Flight flight2 = new Flight("Barcelona", "Athens");
+
+        try {
+            routeFinder.findFilght(flight2);
         } catch (RouteNotFoundException e) {
             System.out.println(e);
         } finally {
